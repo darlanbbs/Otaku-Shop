9e873fca9ca35c8d0f78e0aa23624bf9c9c77361
Darlan Bomfim <darlanbs05@gmail.com>
2023-09-01 11:06:14 -0300

    Pagination Props

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,6 +1,16 @@
 import React from "react";
 
-const Pagination = ({ currentPage, totalPages, onPageChange }) => {
+interface PaginationProps {
+  currentPage: number;
+  totalPages: number;
+  onPageChange: (page: number) => void;
+}
+
+const Pagination = ({
+  currentPage,
+  totalPages,
+  onPageChange,
+}: PaginationProps) => {
   const pages = Array.from({ length: totalPages }, (_, index) => index + 1);
 
   return (
